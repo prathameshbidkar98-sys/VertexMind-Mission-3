79bc86b322a0d9406217588d53f284eacfd14af8
prathameshbidkar98-sys <prathameshbidkar98@gmail.com>
2026-06-16 14:53:02 +0530

    Add database connection module using Mongoose

---
+++ config/db.js
@@ -0,0 +1,22 @@
+const mongoose=require("mongoose");
+
+
+const connectDB=async()=>{
+
+try{
+
+await mongoose.connect(process.env.MONGO_URL);
+
+console.log("Database Connected");
+
+}
+catch(error){
+
+console.log(error.message);
+
+}
+
+}
+
+
+module.exports=connectDB;
